d7638ee1cbfbe8fb44cf7a5b2aab5a98bc871ee7
gallit <gallit@gmail.com>
2019-08-27 12:34:53 +0200

    Add files via upload

---
+++ GpioWorker/Program.cs
@@ -0,0 +1,61 @@
+﻿using GpioWorker.Bll;
+using GpioWorker.Model.Gpio;
+using GpioWorker.Model.Job;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace GpioWorker
+{
+    class Program
+    {
+        private static WorkerManager worker;
+        static void Main(string[] args)
+        {                        
+            TraceManager.Init(ConfigManager.LogPath, ConfigManager.LogConsole, ConfigManager.LogLevel);
+            TraceManager.Trace("*** App.Start ***");
+
+            try
+            {
+                var stopRequestPath = ConfigManager.StopRequestPath;
+                if (File.Exists(stopRequestPath))
+                {
+                    File.Delete(stopRequestPath);
+                }
+
+                worker = new WorkerManager()
+                {
+                    JobFilePath = ConfigManager.JobPath,
+                    Enabled = true
+                };
+
+                var t = Thread.CurrentThread;
+                while (worker.Enabled && t.ThreadState != ThreadState.Stopped)
+                {
+                    if (File.Exists(stopRequestPath) || t.ThreadState == ThreadState.StopRequested || t.ThreadState == ThreadState.AbortRequested)
+                    {
+                        TraceManager.Trace("StopRequest found");
+                        worker.Enabled = false;
+                        File.Delete(stopRequestPath);
+                    }
+                    else
+                    {
+                        Thread.Sleep(100);
+                    }
+                }
+                TraceManager.Trace("Thread.State = " + t.ThreadState);
+            }
+            catch(Exception e)
+            {
+                TraceManager.Trace($"*** App.Error ***\r\n{e.Message}\r\n{e.StackTrace}");
+            }
+            
+            TraceManager.Trace("*** App.End ***");
+
+            //#warning READLINE
+            //Console.ReadLine();
+        }
+
+    }
+}
---
+++ GpioWorker/Readme.txt
@@ -0,0 +1,5 @@
+﻿SystemCTL
+  https://www.dexterindustries.com/howto/run-a-program-on-your-raspberry-pi-at-startup/
+
+touch /tmp/gpio-worker-service-request.stop
+cat /var/log/GpioWorker/Bot1Worker_20180222.log
